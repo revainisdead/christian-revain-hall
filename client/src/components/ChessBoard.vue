ee9c1c68de10a6efe9707ba04a2820edfc4e2d99
Christian Hall <chall@incytestudios.com>
2026-04-25 21:43:08 -0700

    Finish board until props contain notation, chess overlay draws simple arrow.

--- client/src/components/ChessBoard.vue
+++ client/src/components/ChessBoard.vue
@@ -1,61 +1,66 @@
 <template>
   <div class="board-container">
-    <div class="row row1">
-      <BoardPiece num="11" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="12" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="13" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="14" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="15" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="16" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="17" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="18" :dark="true">
-      </BoardPiece>
+    <div class="board">
+      <div class="row row1">
+        <BoardPiece num="11" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="12" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="13" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="14" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="15" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="16" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="17" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="18" :dark="true">
+        </BoardPiece>
+      </div>
+      <div class="row row2">
+        <BoardPiece num="21" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="22" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="23" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="24" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="25" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="26" :dark="false">
+        </BoardPiece>
+        <BoardPiece num="27" :dark="true">
+        </BoardPiece>
+        <BoardPiece num="28" :dark="false">
+        </BoardPiece>
+      </div>
+      <div class="row row3">
+        <BoardRow :flip="false"></BoardRow>
+      </div>
+      <div class="row row4">
+        <BoardRow :flip="true"></BoardRow>
+      </div>
+      <div class="row row5">
+        <BoardRow :flip="false"></BoardRow>
+      </div>
+      <div class="row row6">
+        <BoardRow :flip="true"></BoardRow>
+      </div>
+      <div class="row row7">
+        <BoardRow :flip="false"></BoardRow>
+      </div>
+      <div class="row row8">
+        <BoardRow :flip="true"></BoardRow>
+      </div>
     </div>
-    <div class="row row2">
-      <BoardPiece num="21" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="22" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="23" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="24" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="25" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="26" :dark="false">
-      </BoardPiece>
-      <BoardPiece num="27" :dark="true">
-      </BoardPiece>
-      <BoardPiece num="28" :dark="false">
-      </BoardPiece>
-    </div>
-    <div class="row row3">
-      <BoardRow :flip="false"></BoardRow>
-    </div>
-    <div class="row row4">
-      <BoardRow :flip="true"></BoardRow>
-    </div>
-    <div class="row row5">
-      <BoardRow :flip="false"></BoardRow>
-    </div>
-    <div class="row row6">
-      <BoardRow :flip="true"></BoardRow>
-    </div>
-    <div class="row row7">
-      <BoardRow :flip="false"></BoardRow>
-    </div>
-    <div class="row row8">
-      <BoardRow :flip="true"></BoardRow>
-    </div>
-  </div>
 
+    <!-- overlay next to board -->
+    <ChessOverlay>
+    </ChessOverlay>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -63,6 +68,9 @@ import { ref } from 'vue';
 
 import BoardPiece from 'components/BoardPiece.vue';
 import BoardRow from 'components/BoardRow.vue';
+
+import ChessOverlay from 'components/ChessOverlay.vue';
+
 
 const test = ref(1);
 
@@ -80,7 +88,13 @@ console.log(test)
 </script>
 
 <style>
+.board-container .board {
+  position: absolute;
+  inset: 0;
+}
+
 .board-container {
+  position: relative;
   width: 400px;
   height: 400px;
   border: 2px solid black;
